7a9dafb2a5b196d4d6a8c8e09e169d1619deb46e
Giovana Morais <giovana.vmorais@gmail.com>
2024-08-05 17:22:47 -0400

    refactor mix

--- script/mix.py
+++ script/mix.py
@@ -14,7 +14,7 @@ import tqdm
 DEFAULT_SR = 44100
 # MAX_TRIES = 10
 
-def select_stems(base_stem=None, **kwargs):
+def select_stems(n_percussive, n_harmonic, data_home, index_file, base_stem=None, **kwargs):
     """
     select stems from the index
 
@@ -34,28 +34,30 @@ def select_stems(base_stem=None, **kwargs):
             tempo_bin from the base stem
     """
 
-    index = pd.read_csv(os.path.join(kwargs["data_home"], kwargs["index_file"]))
+    index = pd.read_csv(os.path.join(data_home, index_file))
 
     # base_stem for now is random if not provided
     if base_stem is not None:
         base_stem = index[index["stem_name"] == base_stem]
-    elif kwargs["n_percussive"] > 0:
+    elif n_percussive > 0:
         base_stem = index[index["sound_class"] == "percussive"].sample()
     else:
         base_stem = index[index["sound_class"] == "harmonic"].sample()
 
+    index.drop(base_stem.index, inplace=True)
+
     base_stem = base_stem.to_dict("records")[0]
+    print(f"TEMPO BIN: {base_stem['tempo_bin']}")
 
     # update n_percussives/n_harmonics
     # @lindsey: what if base_stem is undetermined?
     if base_stem["sound_class"] == "percussive":
-        kwargs["n_percussive"] -= 1
+        n_percussive -= 1
     elif base_stem["sound_class"] == "harmonic":
-        kwargs["n_harmonic"] -= 1
+        n_harmonic -= 1
 
     base_tempo = base_stem["tempo_bin"]
 
-    # TODO: remove base stem from index
     # here is where we can also add tempo octaves
     index_filtered = index[
             (index["tempo_bin"] == base_stem["tempo_bin"])
@@ -63,17 +65,31 @@ def select_stems(base_stem=None, **kwargs):
         ]
 
     # TODO: add instrument checking
+
     # sample percussive
     percussive = []
-    if kwargs["n_percussive"] > 0:
-        percussive = index_filtered.sample(kwargs["n_percussive"]).to_dict("records")
+    if n_percussive > 0:
+        percussive_index = index_filtered[index_filtered["sound_class"] == "percussive"]
+        if len(percussive_index) < n_percussive:
+            raise ValueError("Number of requested percussive stems is smaller than stems available in this tempo_bin")
+
+        percussive = percussive_index.sample(n_percussive).to_dict("records")
 
     # sample harmonic
     harmonic = []
-    if kwargs["n_harmonic"] > 0:
-        harmonic = index_filtered.sample(kwargs["n_harmonic"]).to_dict("records")
+    if n_harmonic > 0:
+        harmonic_index = index_filtered[index_filtered["sound_class"] == "harmonic"]
+        if len(harmonic_index) < n_harmonic:
+            raise ValueError("Number of requested harmonic stems is smaller than stems available in this tempo_bin")
+
+        harmonic = harmonic_index.sample(n_harmonic).to_dict("records")
+        print(len(harmonic))
 
     stems = [base_stem] + percussive + harmonic
+
+    # print(stems)
+    print(f"we want {n_harmonic + n_percussive + 1} stems")
+    print(f"we got {len(stems)} stems")
 
     return stems, base_tempo
 
@@ -140,14 +156,14 @@ def align_stems(stems):
         silence_samples = int(shift_difference * DEFAULT_SR)
         silence = np.zeros(silence_samples)
 
-        s["stem_audio"] = np.pad(s["stretched_audio"], (silence_samples, 0), "constant", constant_values=(0,0))
+        s["audio"] = np.pad(s["stretched_audio"], (silence_samples, 0), "constant", constant_values=(0,0))
 
     return aligned_stems
 
 
-def mix(data_home, duration, stems):
+def mix(duration, stems, strategy="cut"):
     """
-    Creates output folder for mixtures if it does not already exist. Receives final processed
+    Receives final processed
     audios and cuts them all to the length of the shortest audio to ensure there will be no
     silence. Creates a list of the truncated stems and cuts them again to fit desired duration>
     Then adds stems together to create mixture and also writes off each stem as a sound file to
@@ -159,6 +175,11 @@ def mix(data_home, duration, stems):
     data_home(str): path to stems
     duration(float): desired duraiton
     final_audios(list): stretched and padded audios
+    strategy : str
+        strategy to deal with stems shorter than desired mixture duration.
+        * zeros: add silence to the end of the stem (default)
+        * cut: cut all stems to minimum lenght
+        * repeat: repeat stem and cut it to match mixture duration
 
     Returns
     ________
@@ -167,60 +188,40 @@ def mix(data_home, duration, stems):
 
     """
 
-    invalid_mixture = False
+    for s in stems:
+        s["audio"] = librosa.util.fix_length(data=s["audio"], size=int(duration*DEFAULT_SR))
 
-    mixture_folder = os.path.join(data_home, "..", "mixtures")
-    os.makedirs(mixture_folder, exist_ok=True)
+    # final_audios_lengths = []
+    # for stem in final_audios:
+    #     final_audios_lengths.append(len(stem))
 
-    final_audios_lengths = []
-    for stem in final_audios:
-        final_audios_lengths.append(len(stem))
+    # min_length = min(final_audios_lengths)
 
-    min_length = min(final_audios_lengths)
+    # if min_length < duration:
+    #     invalid_mixture = True  # checking min length against duration
 
-    if min_length < duration:
-        invalid_mixture = True  # checking min length against duration
+    # total_length = min_length
+    # start_sample = max(0, total_length - int(duration * DEFAULT_SR))
+    # end_sample = total_length
 
-    total_length = min_length
-    start_sample = max(0, total_length - int(duration * DEFAULT_SR))
-    end_sample = total_length
+    # truncated_stems = []
+    # for audio in final_audios:
+    #     audio = audio[:min_length]
+    #     audio = audio[start_sample:end_sample]
+    #     truncated_stems.append(audio)
 
-    truncated_stems = []
-    for audio in final_audios:
-        audio = audio[:min_length]
-        audio = audio[start_sample:end_sample]
-        truncated_stems.append(audio)
+    # length = len(truncated_stems[0])
+    mixture_audio = np.zeros(int(duration*DEFAULT_SR))  # initialization
 
-    length = len(truncated_stems[0])
-    mixture_audio = np.zeros(length)  # initialization
+    for s in stems:
+        mixture_audio += librosa.util.normalize(s["audio"])
 
-    for stem in truncated_stems:
-        if len(stem) == 0:
-            invalid_mixture = True
-            print("invalid, empty stem")
-        mixture_audio += stem
-
-    if not invalid_mixture:  # if it passes all checks
-        mixture_id = str(uuid.uuid4())
-        individual_output_folder = os.path.join(mixture_folder, mixture_id)
-        os.makedirs(individual_output_folder, exist_ok=True)
-
-        for k in range(0, len(truncated_stems)):
-            sf.write(
-                f"{individual_output_folder}/stem{k+1}.wav",
-                truncated_stems[k],
-                DEFAULT_SR,
-            )
-
-        sf.write(f"{individual_output_folder}/mixture.wav", mixture_audio, DEFAULT_SR)
+    return mixture_audio, stems
 
 
-def generate_mixtures(**kwargs):
+def generate_mixtures(data_home, n_mixtures, n_stems, n_harmonic, n_percussive,
+        duration, index_file, output_folder):
     """
-    Consolidates whole mixture-making process under one function call. Between each step checks
-    to see if mixture is still valid, reruns if not. Runs until desired number of mixtures are
-    created.
-
     Parameters
     ----------
     data_home(str): path to stems
@@ -236,19 +237,22 @@ def generate_mixtures(**kwargs):
     None
     """
 
-    count = 0
-    # max_tries = 0
-    while count < kwargs["n_mixtures"]: # or max_tries < MAX_TRIES:
-        print("trying to fetch a mixture")
+    stems, base_tempo = select_stems(**kwargs)
+    stems = time_stretch(stems, base_tempo)
+    stems = align_stems(stems)
+    mixture, stems = mix(duration, stems)
+    save_mixture(output_folder, mixture, stems)
 
-        stems, base_tempo = select_stems(**kwargs)
-        stems = time_stretch(stems, base_tempo)
-        stems = align_stems(stems)
-        mix(data_home, duration, final_audios)
+    return
 
-        # print("sending valid mixture to folder ...\n")
+def save_mixture(output_folder, mixture, stems):
+    """
+    write mixture to .wav file and metadata to .json file
+    """
+    mixture_id = str(uuid.uuid4())
+    sf.write(f"{os.path.join(output_folder, mixture_id)}.wav", mixture, DEFAULT_SR)
 
-        count += 1
+    return
 
 
 if __name__ == "__main__":
@@ -256,6 +260,12 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--data_home", required=True, help="pathway to where is data is stored"
+    )
+    parser.add_argument(
+        "--output_folder",
+        required=False,
+        default="mixtures",
+        help="folder where to save the mixtures.",
     )
 
     parser.add_argument(
@@ -303,6 +313,8 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+
+    os.makedirs(args.output_folder, exist_ok=True)
 
     if args.n_harmonic + args.n_percussive != args.n_stems:
         args.n_harmonic = args.n_stems // 2
